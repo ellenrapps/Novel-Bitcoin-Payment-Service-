204fa77a89b01fa9428565fe96dac026b1b3f6df
Ellen Red <99109157+ellenrapps@users.noreply.github.com>
2026-09-11 21:53:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ class Home():
     def __init__(self, root):
         self.root = root
         self.wallet_name = 'watch_only_wallet'
-        self.add_default_text = 'Enter Testnet Taproot Public Key'
-        self.check_default_text = 'Enter Testnet Taproot Address' 
+        self.add_default_text = 'Enter Bitcoin Public Key'
+        self.check_default_text = 'Enter Bitcoin Address' 
         self.rpc_host = '127.0.0.1'
         self.rpc_port = 48332
         self.rpc_username = None
@@ -34,16 +34,11 @@ class Home():
         self.main_win_y = (self.main_win_screen_height // 2) - (self.main_win_window_height // 2)
         root.geometry(f'{self.main_win_window_width}x{self.main_win_window_height}+{self.main_win_x}+{self.main_win_y}')
         
-        #Main Logo/Buttons Frame
+        # Main Logo/Buttons Frame
         self.main_logo_buttons_frame = tk.Frame(root, borderwidth=2, bg='#414850')
         self.main_logo_buttons_frame.pack(side='top')
 
-        #Main Logo
-        self.main_logo = tk.PhotoImage(file='nov_logo.png') 
-        self.main_logo_label = tk.Label(self.main_logo_buttons_frame, borderwidth=0, highlightthickness=0, image=self.main_logo)
-        self.main_logo_label.grid(row=0, column=0, padx=(1), rowspan=20)
-
-        #Main Home Button
+        # Main Home Button
         self.main_home_button = tk.Button(self.main_logo_buttons_frame, command=lambda: self.main_home_click(), state='disabled', text= 'Home', bd=2.5, bg='#4f697f', width=3, font=('Segoe', 9, 'bold'))
         self.main_home_button.grid(row=1, column=2)
 
@@ -103,45 +98,50 @@ class Home():
         self.explorer_log_pass.bind('<FocusIn>', self.pass_focus_in)
         self.explorer_log_pass.bind('<FocusOut>', self.pass_focus_out)
         self.explorer_log_pass.pack(side=tk.LEFT, padx=(0, 2), fill=tk.BOTH)
-        self.explorer_log_connect_btn = tk.Button(self.explorer_log_frame, width=27, command=self.connect_click, text='Connect to Local Bitcoin Node', bg='#4f697f', fg='#f7931a', font=('Arial', 9))
+        self.explorer_log_connect_btn = tk.Button(self.explorer_log_frame, bd=1.5, width=27, command=self.connect_click, text='Connect to Local Bitcoin Node', bg='#4f697f', fg='#f7931a', font=('Arial', 9))
         self.explorer_log_connect_btn.pack(side=tk.LEFT, padx=(0, 1), fill=tk.BOTH)
-        self.explorer_log_wallet_btn = tk.Button(self.explorer_log_frame, width=19, state='disable', command=self.load_wallet_click, text='Load Watch-Only Wallet', bg='#4f697f', fg='#f7931a', font=('Arial', 9))
+        self.explorer_log_wallet_btn = tk.Button(self.explorer_log_frame, bd=1.5, width=19, state='disable', command=self.load_wallet_click, text='Load Watch-Only Wallet', bg='#4f697f', fg='#f7931a', font=('Arial', 9))
         self.explorer_log_wallet_btn.pack(side=tk.LEFT, padx=(0, 6), fill=tk.BOTH)
         
         # Explorer Add Public Key
         self.explorer_add_pubkey_frame = tk.Frame(self.explorer_outer_frame, bg='#414850')
         self.explorer_add_pubkey_frame.pack(padx=3, fill=tk.X)
-        self.explorer_add_pubkey_ent = tk.Entry(self.explorer_add_pubkey_frame, justify="center", font=('Arial', 9), width=68)
+        self.explorer_add_pubkey_ent = tk.Entry(self.explorer_add_pubkey_frame, justify="center", font=('Arial', 9), width=60)
         self.explorer_add_pubkey_ent.insert(0, self.add_default_text)
         self.explorer_add_pubkey_ent.pack(side=tk.LEFT, padx=(6, 1), pady=(0, 1), fill=tk.BOTH)
         self.explorer_add_pubkey_ent.config(state='disabled')
-        self.explorer_add_pubkey_ent.bind("<FocusIn>", self.on_entry_focus)
-        self.explorer_add_pubkey_btn = tk.Button(self.explorer_add_pubkey_frame, state='disabled', text='Add Public Key', command=self.add_pubkey_clicked, bg='#4f697f', fg='#f7931a', font=('Arial', 9))
+        self.explorer_add_pubkey_ent.bind('<FocusIn>', self.on_entry_focus)
+        self.explorer_add_pubkey_btn = tk.Button(self.explorer_add_pubkey_frame, bd=1.5, state='disabled', text='Add to Watch-Only Wallet', command=self.add_pubkey_clicked, bg='#4f697f', fg='#f7931a', font=('Arial', 9))
         self.explorer_add_pubkey_btn.pack(side=tk.LEFT, padx=(0, 6), fill=tk.Y)
         
-        # Explorer Check Address
-        self.explorer_check_address_frame = tk.Frame(self.explorer_outer_frame, bg='#414850')
-        self.explorer_check_address_frame.pack(padx=3, fill=tk.X)
-        self.explorer_check_address_ent = tk.Entry(self.explorer_check_address_frame, justify="center", font=('Arial', 9), width=68)
+        # Explorer Check Balance, Transaction & Messages
+        self.explorer_check_frame = tk.Frame(self.explorer_outer_frame, bg='#414850')
+        self.explorer_check_frame.pack(padx=3, fill=tk.X)
+        self.explorer_check_address_ent = tk.Entry(self.explorer_check_frame, justify="center", font=('Arial', 9), width=31)
         self.explorer_check_address_ent.insert(0, self.check_default_text)        
         self.explorer_check_address_ent.pack(side=tk.LEFT, padx=(6, 1), pady=(0, 1), fill=tk.BOTH)
         self.explorer_check_address_ent.config(state='disabled')
-        self.explorer_check_address_ent.bind("<FocusIn>", self.on_entry_focus)
-        self.explorer_check_address_btn = tk.Button(self.explorer_check_address_frame, state='disabled', text='Check Address', command=self.scan_address_clicked, bg='#4f697f', fg='#f7931a', font=('Arial', 9))
-        self.explorer_check_address_btn.pack(side=tk.LEFT, padx=(0, 6), fill=tk.Y)
+        self.explorer_check_address_ent.bind('<FocusIn>', self.on_entry_focus)
+        self.explorer_check_balance_tx_btn = tk.Button(self.explorer_check_frame, bd=1.5, state='disabled', text='Check Address Balance & Transactions', command=self.check_balance_clicked, bg='#4f697f', fg='#f7931a', font=('Arial', 9))
+        self.explorer_check_balance_tx_btn.pack(side=tk.LEFT, fill=tk.Y)
+        self.explorer_check_messages_btn = tk.Button(self.explorer_check_frame, bd=1.5, state='disabled', text='Check Messages', bg='#4f697f', fg='#f7931a', font=('Arial', 9))
+        self.explorer_check_messages_btn.pack(side=tk.LEFT, padx=(0, 6), fill=tk.Y)
         
         # Explorer Textbox
-        self.results_label = tk.Label(self.explorer_outer_frame, text="Results/Notifications", anchor="w", bg='#414850', fg='white', font=("Arial", 9))
+        self.results_label = tk.Label(self.explorer_outer_frame, text='Results/Notifications', anchor="w", bg='#414850', fg='white', font=("Arial", 9))
         self.results_label.pack(fill='x', padx=6, pady=7)
-        self.explorer_textbox_frame = tk.Frame(self.explorer_outer_frame, bg='#414850')
+        self.explorer_textbox_frame = tk.Frame(self.explorer_outer_frame)
         self.explorer_textbox_frame.pack() 
-        self.explorer_textbox = tk.Text(self.explorer_textbox_frame, font=('Segoe', 11))
-        self.explorer_textbox = scrolledtext.ScrolledText(self.explorer_textbox_frame, wrap=tk.WORD, height=14)
+        self.explorer_textbox = tk.Text(self.explorer_textbox_frame, bg='#414850', fg='white', font=("Segoe", 11))
+        self.explorer_textbox = scrolledtext.ScrolledText(self.explorer_textbox_frame, bg='#414850', fg="white", font=("Segoe", 11), wrap=tk.WORD, width=68, height=11)
         self.explorer_textbox.tag_configure('center_tag', justify='center')
         self.explorer_textbox.tag_add('center_tag', '1.0', '1.end')
         self.explorer_textbox.bind('<Up>', self.privacy_move_up)
         self.explorer_textbox.bind('<Down>', self.privacy_move_down)
-        self.explorer_textbox.pack(pady=(4), padx=10, anchor='w')
+        self.explorer_textbox.pack(pady=(8), padx=(8), anchor='w')
+        self.nov_logo = tk.PhotoImage(file='nov_logo.png')
+        self.explorer_textbox.image_create('end', image=self.nov_logo)
+        self.explorer_textbox.image = self.nov_logo
 
         #################
         # Address Widgets
@@ -220,7 +220,7 @@ class Home():
         # Privacy Textbox
         self.privacy_textbox = tk.Text(self.privacy_inner_frame, height=23, font=('Segoe', 11))
         self.privacy_textbox = scrolledtext.ScrolledText(self.privacy_inner_frame, wrap=tk.WORD, height=23)
-        self.privacy_textbox.insert(tk.END, 'P R I V A C Y / T E R M S / F A Q\n\nPRIVACY POLICY\nNovel Bitcoin Payment Service respects your privacy. It doesn’t access, collect, or transmit any information (data that could be used to identify a person). As a user of this service, you have full control over your bitcoin address and private key. What this means is that you are fully responsible for securing your bitcoin address and private key and you have full control over your bitcoin funds.\n\nTERMS OF SERVICE\nBy using this Novel Bitcoin Payment Service, you agree that you will not use this service to commit illegal activities within and outside your geographical location.\n\nFREQUENTLY ASKED QUESTION: What is Novel Bitcoin Payment Service?\nBitcoin Payment Service aims to be your very own payment service, attaining the goal of Peer-to-Peer Electronic Cash System as envisioned by Satoshi Nakamoto.\n\nIn the Bitcoin white paper written by Nakamoto, Peer-to-Peer Electronic Cash System is described as allowing online payments to be sent directly from one party to another without going through a financial institution.\n\nThis payment service does not aim to be a financial institution. Rather, this payment service aims to be a means for ordinary folks to send bitcoin, whether as payment or donation, to one another, that is, without an intermediary financial institution.\n\nSpecifically, this payment service will allow everyday users to:\n1. Create bitcoin address + key\n2. Create smart contracts\n3. Check bitcoin address balance using an in-built blockchain explorer\n4. Receive bitcoin\n5. Send bitcoin\n6. Communicate regarding the received or sent satoshis within the Bitcoin blockchain\n\nUnknown to many, Nakamoto built within the Bitcoin infrastracture a means for senders and receivers to communicate with each other. Nakamoto also built within the Bitcoin infrastracture smart contracts. These in-built communication system and smart contracts within the Bitcoin blockchain will be put into use in this novel Bitcoin Payment System.')  
+        self.privacy_textbox.insert(tk.END, '')  
         self.privacy_textbox.tag_configure('center_tag', justify='center')
         self.privacy_textbox.tag_add('center_tag', '1.0', '1.end')
         self.privacy_textbox.bind('<Up>', self.privacy_move_up)
@@ -275,58 +275,14 @@ class Home():
     ####################
     # Explorer Functions
     ####################
-    def copy_text(self):
-        if self.explorer_textbox.tag_ranges('sel'):
-            selected = self.explorer_textbox.get('sel.first', 'sel.last')
-            self.root.clipboard_clear()
-            self.root.clipboard_append(selected)
-            self.copy_text_showinfo = f'Text Copied'
-            self.display_result(self.copy_text_showinfo)
-        else:
-            all_text = self.explorer_textbox.get('1.0', tk.END).rstrip('\n')
-            if all_text:
-                self.root.clipboard_clear()
-                self.root.clipboard_append(all_text)                
-            else:
-                self.copy_text_showwarning = f'Nothing to copy. Textbox is empty.'
-                self.display_result(self.copy_text_showwarning)
-        
-        copy_text_btn_frame = tk.Frame(
-            self.root,
-            bg='#e0f7fa',          
-            relief='raised',
-            bd=2.5
-        )
-
-        copy_icon_label = tk.Label(
-            copy_text_btn_frame,
-            text='📋',
-            font=('Arial', 10),
-            fg='#0277bd',  
-            bg='#e0f7fa'
-        )
-        copy_icon_label.pack(side='left')
-        
-        copy_text_label = tk.Label(
-            copy_text_btn_frame,
-            text='Copy',
-            font=('Arial', 9, 'bold'),
-            fg='#0d47a1',         
-        )
-
-        copy_text_label.pack(side='left')
-        copy_text_btn_frame.bind('<Button-1>', lambda e: self.copy_text())
-        copy_text_btn_frame.bind('<Enter>', lambda e: copy_text_btn_frame.config(bg='#99d2f2'))
-        copy_text_btn_frame.bind('<Leave>', lambda e: copy_text_btn_frame.config(bg='#e0f7fa'))
-        copy_text_btn_frame.place(in_=self.explorer_textbox, relx=1.0, rely=0.0, anchor='ne', x=-14)
-
-
     def display_result(self, text):
         self.explorer_textbox.config(state='normal')
         self.explorer_textbox.delete(1.0, tk.END)
-        self.explorer_textbox.insert(tk.END, "\n" + text)
+        self.nov_logo = tk.PhotoImage(file='nov_logo.png')
+        self.explorer_textbox.image_create('end', image=self.nov_logo)
+        self.explorer_textbox.image = self.nov_logo
+        self.explorer_textbox.insert(tk.END, text)
         self.explorer_textbox.see('1.0')
-        self.copy_text()
 
     
     def clear_result(self):
@@ -343,7 +299,7 @@ class Home():
             if other_ent.get() == '':
                 other_ent.insert(0, self.check_default_text)
                 
-            self.explorer_check_address_btn.config(state='disabled')
+            self.explorer_check_balance_tx_btn.config(state='disabled')
             self.explorer_add_pubkey_btn.config(state='normal')            
                 
         elif widget == self.explorer_check_address_ent:
@@ -353,13 +309,15 @@ class Home():
                 other_ent.insert(0, self.add_default_text)
                 
             self.explorer_add_pubkey_btn.config(state='disabled')
-            self.explorer_check_address_btn.config(state='normal')
+            self.explorer_check_balance_tx_btn.config(state='normal')
 
             
     def user_focus_in(self, event):
         if self.explorer_log_user.get() == 'Enter Node User':
             self.explorer_log_user.delete(0, 'end') 
             self.explorer_log_user.insert(0, '') 
+            # Hides username
+            self.explorer_log_user.config(show='*')
 
 
     def user_focus_out(self, event):
@@ -414,7 +372,6 @@ class Home():
         result_blockchain_info = rpc_calls.get_blockchain_info(
             self.rpc_host,
             self.rpc_port,
-            self.rpc_username,
             self.rpc_auth_header
         )
 
@@ -444,7 +401,6 @@ class Home():
         result_wallet_creation = rpc_calls.create_or_load_wallet(
                 self.rpc_host,
                 self.rpc_port,
-                self.rpc_username,
                 self.rpc_auth_header,
                 self.wallet_name
                 )
@@ -456,7 +412,7 @@ class Home():
             self.explorer_add_pubkey_ent.config(state='normal')
             self.explorer_add_pubkey_btn.config(state='normal')
             self.explorer_check_address_ent.config(state='normal')
-            self.explorer_check_address_btn.config(state='normal')
+            self.explorer_check_balance_tx_btn.config(state='normal')
                 
         else:
             result_wallet_creation_error = f'{result_wallet_creation}.\n\n'
@@ -474,7 +430,6 @@ class Home():
             result_add_pubkey = rpc_calls.rpc_add_pubkey(
                 self.rpc_host,
                 self.rpc_port,
-                self.rpc_username,
                 self.rpc_auth_header,
                 self.pubkey_hex,
                 label='#1'
@@ -506,17 +461,17 @@ class Home():
 
         finally:
             self.explorer_add_pubkey_btn.config(state='normal')
+            self.explorer_check_balance_tx_btn.config(state='normal')
             self.explorer_add_pubkey_ent.config(bg='white')
 
-    ###############
-    # Check Address
-    ###############
+    ##############################
+    # Check Balance + Transactions
+    ##############################
     def run_rpc_in_thread(self, address: str) -> None:
         try:
             result_balance_transactions = rpc_calls.fetch_latest_balance_transactions(
                 self.rpc_host,
                 self.rpc_port,
-                self.rpc_username,
                 self.rpc_auth_header,
                 address,
                 limit=5
@@ -531,15 +486,14 @@ class Home():
                     f"Address: {data['address']}\n"
                     f"Total Balance: {data['total_balance']} tBTC\n"
                     f"Confirmed Transactions Found: {data['confirmed_transactions_found']}\n"
-                    f"Showing Latest Confirmed Transactions: {len(data['transactions'])}\n"
+                    f"Latest confirmed transactions are listed below: {len(data['transactions'])}\n"
                 )
-                full_output += summary + "\n"
+                full_output += summary + '\n'
 
                 for i, tx in enumerate(data['transactions'], start=1):
                     tx_info = (
                         f"--- Transaction #{i} ---\n"
                         f"TXID: {tx['txid']}\n"
-                        f"Block Height: {tx.get('blockheight')}\n"
                         f"Confirmations: {tx.get('confirmations')}\n"
                         f"Time: {tx.get('time')}\n"
                         f"Inputs ({len(tx['inputs'])}):\n"
@@ -553,11 +507,8 @@ class Home():
 
                     tx_info += f"Outputs ({len(tx['outputs'])}):\n"
                     for j, vout in enumerate(tx['outputs']):
-                        spk = vout['scriptPubKey']
-                        addrs = ','.join(spk.get('addresses', [])) or '(no addresses)'
                         tx_info += (
-                            f"  [{j}] value={vout['value']} tBTC, type={spk['type']}, "
-                            f"addresses=[{addrs}]\n"
+                            f"  [{j}] value={vout['value']} tBTC\n"
                         )
                     
                     full_output += tx_info + '\n'
@@ -573,21 +524,22 @@ class Home():
             self.root.after(0, self.display_result, gen_error_msg)
 
         finally:
-            self.root.after(0, lambda: self.explorer_check_address_btn.config(state='normal'))
+            self.root.after(0, lambda: self.explorer_check_balance_tx_btn.config(state='normal'))
             self.root.after(0, lambda: self.explorer_check_address_ent.config(bg='white'))
 
 
-    def scan_address_clicked(self) -> None:
+    def check_balance_clicked(self) -> None:
         address = self.explorer_check_address_ent.get().strip()
         self.explorer_check_address_ent.delete(0, 'end')
         self.explorer_check_address_ent.insert(0, self.add_default_text)
         self.explorer_check_address_ent.config(bg='light gray')
-        self.explorer_check_address_btn.config(state='disabled')
+        self.explorer_check_balance_tx_btn.config(state='disabled')
 
         self.display_result('Processing ...')
 
         thread = threading.Thread(target=self.run_rpc_in_thread, args=(address,), daemon=True)
         thread.start()
+
 
     ###################
     # Address Functions
